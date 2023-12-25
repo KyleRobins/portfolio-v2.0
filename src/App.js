093b8f635a9e3881a0fd0b4cc89274c33e6d9948
Kyle Robins <robinsmicky@gmail.com>
2023-12-25 21:06:22 +0300

    Web Structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import { darkTheme } from "./utils/Themes";
+import Navabar from './components/Navbar';
+import Hero from './components/HeroSection';
+import Skills from './components/Skills';
+const Body = styled.div` 
+background-color: ${({ theme }) => theme.bg};
+width: 100%;
+height: 100%;
+overflow-x: hidden;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Navabar/>
+      <Body>Portfolio</Body>
+      <Hero/>
+      <Skills/>
+    </ThemeProvider>
+    
   );
 }
 
