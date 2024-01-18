d74a490001d72999440cd66caeed8dee0bfabfba
kylerobins <robinsmicky@gmail.com>
2024-01-18 21:10:26 +0300

    Current Stats Updates

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -22,21 +22,21 @@ const Projects = ({openModal,setOpenModal}) => {
           }
           <Divider />
           {toggle === 'web app' ?
-            <ToggleButton active value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
+            <ToggleButton active value="web app" onClick={() => setToggle('web app')}>WEB</ToggleButton>
             :
-            <ToggleButton value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
+            <ToggleButton value="web app" onClick={() => setToggle('web app')}>WEB</ToggleButton>
           }
           <Divider />
           {toggle === 'android app' ?
-            <ToggleButton active value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
+            <ToggleButton active value="android app" onClick={() => setToggle('android app')}>DEVOPS</ToggleButton>
             :
-            <ToggleButton value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
+            <ToggleButton value="android app" onClick={() => setToggle('android app')}>DEVOPS</ToggleButton>
           }
           <Divider />
           {toggle === 'machine learning' ?
-            <ToggleButton active value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
+            <ToggleButton active value="machine learning" onClick={() => setToggle('machine learning')}>GRAPHICS</ToggleButton>
             :
-            <ToggleButton value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
+            <ToggleButton value="machine learning" onClick={() => setToggle('machine learning')}>GRAPHICS</ToggleButton>
           }
         </ToggleButtonGroup>
         <CardContainer>
@@ -45,7 +45,7 @@ const Projects = ({openModal,setOpenModal}) => {
               <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
             ))}
           {projects
-            .filter((item) => item.category == toggle)
+            .filter((item) => item.category === toggle)
             .map((project) => (
               <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
             ))}
--- src/data/constants.js
+++ src/data/constants.js
@@ -127,6 +127,21 @@ export const skills = [
           "https://res.cloudinary.com/dzw5pskk1/image/upload/v1705569834/figma_abiof3.svg",
       },
       {
+        name: "Sketch",
+        image:
+          "https://res.cloudinary.com/dzw5pskk1/image/upload/v1705570852/sketch_k4jtuj.svg",
+      },
+      {
+        name: "Canva",
+        image:
+          "https://imgs.search.brave.com/JolF_soQvMaevft791In1tQQGFMq_YqisFAtL-zJIMI/rs:fit:32:32:1/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvZjg4ZDcxYmQ2/ZjkzMGEyNjUyNGJh/MjY0YjI4YmJmMWQz/YTcwNWIwOTFjNzUx/YmNlMWNmNmFkYTJl/NDA3YTdmMy93d3cu/Y2FudmEuY29tLw",
+      },
+      {
+        name: "Capcut",
+        image:
+          "https://imgs.search.brave.com/KDw8x0Nr1eXZbzZGvtaD1dWktm8VlhgvQZezv6lNCvo/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9mcmVl/bG9nb3BuZy5jb20v/aW1hZ2VzL2FsbF9p/bWcvMTY2NDI4NDgz/NmNhcC1jdXQtbG9n/by1wbmcucG5n",
+      },
+      {
         name: "Adobe Illustrator",
         image:
           "https://res.cloudinary.com/dzw5pskk1/image/upload/v1705568954/adobe-illustrator_brjq6y.svg",
@@ -140,11 +155,6 @@ export const skills = [
         name: "Adobe Photoshop",
         image:
           "https://res.cloudinary.com/dzw5pskk1/image/upload/v1705568962/adobe-photoshop_igoo3o.svg",
-      },
-      {
-        name: "Capcut",
-        image:
-          "https://imgs.search.brave.com/KDw8x0Nr1eXZbzZGvtaD1dWktm8VlhgvQZezv6lNCvo/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9mcmVl/bG9nb3BuZy5jb20v/aW1hZ2VzL2FsbF9p/bWcvMTY2NDI4NDgz/NmNhcC1jdXQtbG9n/by1wbmcucG5n",
       },
     ],
   },
@@ -201,13 +211,13 @@ export const experiences = [
     role: "DevOps & Fullstack Engineer",
     company: "Elitcorp ltd.",
     date: "Present",
-    desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+    desc: "Optimizing IT infrastructure, specializing in cloud technologies, automation, and DevOps best practices.Currently contributing to DevOps practices at ElitCorp, focusing on enhancing operational efficiency, automating workflows, and optimizing cloud environments. Committed to ongoing learning and staying abreast of industry trends.",
     skills: [
       "Docker",
       "Terraform",
       "AWS",
       "EC2",
-      "Portainer",
+      "Kubernetes",
       "Nginx",
       "JavaScript",
       "TypeScript",
@@ -217,16 +227,18 @@ export const experiences = [
   },
   {
     id: 0,
-    img: "https://res.cloudinary.com/dzw5pskk1/image/upload/v1705580521/1630534015323_dc2cre.jpg",
+    img: "https://res.cloudinary.com/dzw5pskk1/image/upload/v1705600457/unistus-new_qew4lo.png",
     company: "Unistus Services Ltd",
     date: "Aug 2022 - Dec 2023",
-    desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+    desc: "Led skilled teams in web development, DevOps, graphic design, and IT consulting, fostering collaboration and inspiring excellence. Oversaw end-to-end project lifecycles for on-time delivery and client satisfaction. Recommended cutting-edge tools and methodologies to enhance service efficiency and quality. Provided expert IT consulting services, consistently exceeding client expectations by understanding and meeting their needs.",
     skills: [
       "ReactJS",
       "Nginx",
       "NodeJs",
       "Digital Ocean",
       "HTML",
+      "Adobe Illustrator",
+      "Adobe Photoshop",
       "CSS",
       "JavaScript",
       "Wordpress",
@@ -237,8 +249,8 @@ export const experiences = [
     img: "https://res.cloudinary.com/dzw5pskk1/image/upload/v1705581205/nitrogen_w6ryxr.png",
     role: "Front-End Developer",
     company: "Nitrogen Technologies",
-    date: "Feb 2022 - June 2022",
-    desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+    date: "Jan 2022 - May 2022",
+    desc: "Contributed to a payments gateway content management system (CMS) project using React, while also designing the company's landing page using WordPress. Additionally, played a role in developing mobile UI and UX designs.",
     skills: [
       "ReactJS",
       "Redux",
@@ -257,11 +269,18 @@ export const experiences = [
     img: "https://res.cloudinary.com/dzw5pskk1/image/upload/v1705581617/Muru-Tech-Blue-Favicon-e1671027552148_d8lyzf.webp",
     role: "Front-End Developer Intern",
     company: "Murutech Inc.",
-    date: "May 2023 - Present",
-    desc: "Contributing to different open-source projects and learn from industry experts",
-    doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
+    date: "Jan 2021 - July 2021",
+    desc: "Specialized in custom WordPress development, creating themes and plugins to enhance website functionality. Implemented web analytics tools to measure and analyze website performance and user behavior, providing data-driven insights for future improvements",
+    skills: [
+      "Wordpress",
+      "MySQL",
+      "React js",
+      "Material UI",
+      "Figma",
+      "AWS",
+      "Nginx",
+    ],
   },
-  
 ];
 
 export const education = [
@@ -288,64 +307,58 @@ export const education = [
 export const projects = [
   {
     id: 9,
-    title: "Trackify",
+    title: "Senke",
     date: "Jun 2023 - Jul 2023",
     description:
       "Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. \nAdmin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials:	#Email: testemployee@gmail.com	#Password- 123@Testemployee",
     image:
-      "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
+      "https://res.cloudinary.com/dzw5pskk1/image/upload/v1705588877/senkeafrca_sbgjkb.png",
     tags: [
-      "Docker",
-      "AWS",
-      "DuckDNS",
-      "Eslint",
-      "Husky",
-      "CI/CD",
+      "Next Js",
+      "Mysql",
+      "Vercel",
+      "Nginx",
+      "Firebase",
+      "Github Actions",
       "React Js",
-      "MongoDb",
       "Node Js",
-      "Express Js",
-      "Redux",
     ],
     category: "web app",
-    github: "https://github.com/rishavchanda/Trackify",
-    webapp: "https://trackify.duckdns.org",
+    github: "#",
+    webapp: "https://senkeafrica.com",
   },
   {
     id: 0,
-    title: "Podstream",
+    title: "Senke Dashboard",
     date: "Apr 2023 - May 2023",
     description:
       "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
     image:
-      "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
-    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
-    category: "web app",
-    github: "https://github.com/rishavchanda/Podstream",
-    webapp: "https://podstream.netlify.app/",
-    member: [
-      {
-        name: "Rishav Chanda",
-        img: "https://avatars.githubusercontent.com/u/64485885?v=4",
-        linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-        github: "https://github.com/rishavchanda/",
-      },
-      {
-        name: "Upasana Chaudhuri",
-        img: "https://avatars.githubusercontent.com/u/100614635?v=4",
-        linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
-        github: "https://github.com/upasana0710",
-      },
+      "https://res.cloudinary.com/dzw5pskk1/image/upload/v1705589332/senke-admin_fl6xow.png",
+    tags: [
+      "React Js",
+      "MySql",
+      "Laravel",
+      "Firebase",
+      "SMTP",
+      "Nginx",
+      "Gcp",
+      "Cpanel",
+      "Vercel",
+      "Nodejs",
     ],
+    category: "web app",
+    github: "#",
+    webapp: "admin.senkeafrica.com",
   },
   {
     id: 1,
-    title: "Vexa",
+    title: "Elitcorp",
     date: "Oct 2022 - Present",
     description:
       "Designed and developed the Vexa project, a project management app that helps users and teams stay organized and on track. Implemented key features such as task tracking, team collaboration, and time tracking to improve productivity and project outcomes and also invite team/project members. The plan is to add a community of users where they can find developers and other team members and chat system implementation.",
     image:
-      "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+      "https://res.cloudinary.com/dzw5pskk1/image/upload/v1705590482/elit_ajmdvv.png",
     tags: [
       "React Js",
       "MongoDb",
@@ -413,25 +426,25 @@ export const projects = [
   },
   {
     id: 4,
-    title: "Whatsapp Clone",
+    title: "Personal Blog",
     date: "Jul 2021",
     description:
       "A WhatsApp clone made with React JS, Firebase, and Material UI. It has Phone Authentication, Real-time Database. It has a chat room where users can chat with each other. It has a sidebar where users can see all the chat rooms and can create a new chat room. It has a login page where users can log in with their Google account.",
     image:
-      "https://firebasestorage.googleapis.com/v0/b/whatsapp-clone-rishav.appspot.com/o/Screenshot%20(151).png?alt=media&token=48391593-1ef0-4a8c-a92a-eb82bdf38e89",
+      "https://res.cloudinary.com/dzw5pskk1/image/upload/v1705593640/blog_vrzuq3.png",
     tags: ["React Js", "Firebase", "Firestore", "Node JS"],
     category: "web app",
-    github: "https://github.com/rishavchanda/Whatsapp-Clone-React-Js",
-    webapp: "https://whatsapp-clone-rishav.web.app",
+    github: "#",
+    webapp: "https://blog.kylerobins.com",
   },
   {
     id: 5,
-    title: "Todo Web App",
+    title: "Pgemsfresh",
     date: "Jun 2021",
     description:
       " A Todo Web App made with React JS, Redux, and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
     image:
-      "https://camo.githubusercontent.com/a328255ad96f861f57d25096d28018ab2656c689a1456b0d145764009bed2d1a/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
+      "https://res.cloudinary.com/dzw5pskk1/image/upload/v1705593968/pgemsfresh_lxmi1q.png",
     tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
     category: "web app",
     github: "https://github.com/rishavchanda/Todo-Web-App",
@@ -439,16 +452,17 @@ export const projects = [
   },
   {
     id: 6,
-    title: "Breaking Bad",
+    title: "Unistus Ltd CRM",
     date: "Jun 2021",
     description:
       "A simple react app that shows the characters of the famous TV series Breaking Bad. It uses the Breaking Bad API to fetch the data. It also has a search bar to search for a particular character.",
     image:
-      "https://camo.githubusercontent.com/cd07010cbeb90cb1b43a5d6162784326aef02210ef7d41a0f9ae043b3e392378/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f7425323028313534292e706e673f616c743d6d6564696126746f6b656e3d65613439383630632d303435362d343333342d616435372d336239346663303333363263",
-    tags: ["React Js", "API", "Axios", "Node JS"],
+      "https://res.cloudinary.com/dzw5pskk1/image/upload/v1705597419/unistusodoo_pbrxa5.png",
+    tags: ["Python", "API", "Docker", "Node JS", "Nginx", "MySql", "Odoo"],
     category: "web app",
-    github: "https://github.com/rishavchanda/Breaking-Bad",
-    webapp: "https://breaking-bad-webapp.netlify.app",
+    github:
+      "https://github.com/KyleRobins/Scripts/blob/main/Odoo/openhrms_install.sh",
+    webapp: "https://hrm.unistusltd.co.ke",
   },
   {
     id: 7,
