c0530fa30ed123d544550a94cce5a4a24c5a08c8
kylerobins <robinsmicky@gmail.com>
2024-02-04 08:14:47 +0300

    Contact Form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,45 +1,63 @@
-import React, { useRef, useState } from "react";
-import styled from "styled-components";
-import emailjs from "@emailjs/browser";
-import { Snackbar } from "@mui/material";
+import React from 'react'
+import styled from 'styled-components'
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { Snackbar } from '@mui/material';
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  position: relative;
-  z-index: 1;
-  align-items: center;
-`;
+display: flex;
+flex-direction: column;
+justify-content: center;
+position: relative;
+z-index: 1;
+align-items: center;
+@media (max-width: 960px) {
+    padding: 0px;
+}
+`
 
 const Wrapper = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: column;
-  width: 100%;
-  max-width: 600px; /* Adjust this to match your design */
-  padding: 0px 0px 80px 0px;
-  gap: 12px;
-`;
+position: relative;
+display: flex;
+justify-content: space-between;
+align-items: center;
+flex-direction: column;
+width: 100%;
+max-width: 1350px;
+padding: 0px 0px 80px 0px;
+gap: 12px;
+@media (max-width: 960px) {
+    flex-direction: column;
+}
+`
 
 const Title = styled.div`
-  font-size: 24px;
-  text-align: center;
-  font-weight: 600;
-  margin-top: 20px;
+font-size: 42px;
+text-align: center;
+font-weight: 600;
+margin-top: 20px;
   color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 768px) {
+      margin-top: 12px;
+      font-size: 32px;
+  }
 `;
 
 const Desc = styled.div`
-  font-size: 16px;
-  text-align: center;
-  color: ${({ theme }) => theme.text_secondary};
+    font-size: 18px;
+    text-align: center;
+    max-width: 600px;
+    color: ${({ theme }) => theme.text_secondary};
+    @media (max-width: 768px) {
+        margin-top: 12px;
+        font-size: 16px;
+    }
 `;
 
+
 const ContactForm = styled.form`
-  width: 100%;
+  width: 95%;
+  max-width: 600px;
   display: flex;
   flex-direction: column;
   background-color: ${({ theme }) => theme.card};
@@ -48,130 +66,104 @@ const ContactForm = styled.form`
   box-shadow: rgba(23, 92, 230, 0.15) 0px 4px 24px;
   margin-top: 28px;
   gap: 12px;
-`;
+`
 
 const ContactTitle = styled.div`
-  font-size: 18px;
+  font-size: 24px;
   margin-bottom: 6px;
   font-weight: 600;
   color: ${({ theme }) => theme.text_primary};
-`;
+`
 
 const ContactInput = styled.input`
+  flex: 1;
   background-color: transparent;
   border: 1px solid ${({ theme }) => theme.text_secondary};
   outline: none;
-  font-size: 16px;
+  font-size: 18px;
   color: ${({ theme }) => theme.text_primary};
   border-radius: 12px;
   padding: 12px 16px;
   &:focus {
     border: 1px solid ${({ theme }) => theme.primary};
   }
-`;
+`
 
 const ContactInputMessage = styled.textarea`
+  flex: 1;
   background-color: transparent;
   border: 1px solid ${({ theme }) => theme.text_secondary};
   outline: none;
-  font-size: 16px;
+  font-size: 18px;
   color: ${({ theme }) => theme.text_primary};
   border-radius: 12px;
   padding: 12px 16px;
   &:focus {
     border: 1px solid ${({ theme }) => theme.primary};
   }
-`;
+`
 
-const ContactButton = styled.button`
+const ContactButton = styled.input`
   width: 100%;
+  text-decoration: none;
   text-align: center;
-  background: linear-gradient(
-    225deg,
-    hsla(271, 100%, 50%, 1) 0%,
-    hsla(294, 100%, 50%, 1) 100%
-  );
+  background: hsla(271, 100%, 50%, 1);
+  background: linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+  background: -moz-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+  background: -webkit-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
   padding: 13px 16px;
   margin-top: 2px;
   border-radius: 12px;
   border: none;
   color: ${({ theme }) => theme.text_primary};
-  font-size: 16px;
+  font-size: 18px;
   font-weight: 600;
-  cursor: pointer;
-`;
+`
+
+
 
 const Contact = () => {
-  const [open, setOpen] = useState(false);
+
+  //hooks
+  const [open, setOpen] = React.useState(false);
   const form = useRef();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    // Extract form data
-    const formData = new FormData(form.current);
-    const data = {};
-    formData.forEach((value, key) => {
-      data[key] = value;
-    });
-
-    // Send form data to your Resend API
-    fetch("/api/send/route", {
-      // Adjust this endpoint to your setup
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        to: data.from_email, // Adjust based on how your API expects data
-        subject: data.subject,
-        firstName: data.from_name, // Make sure the property matches your email template's expected prop
-        message: data.message, // Assuming your email template can handle a message field
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.success) {
-          setOpen(true); // Show success message
-          form.current.reset(); // Reset form fields
-        } else {
-          // Handle error - you might want to adjust based on your API response
-          console.error("Failed to send email:", data.error);
-        }
-      })
-      .catch((error) => {
-        console.error("Error submitting form:", error);
+    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+      .then((result) => {
+        setOpen(true);
+        form.current.reset();
+      }, (error) => {
+        console.log(error.text);
       });
-  };
+  }
+
+
 
   return (
     <Container>
       <Wrapper>
         <Title>Contact</Title>
-        <Desc>
-          Feel free to reach out to me for any questions or opportunities!
-        </Desc>
+        <Desc>Feel free to reach out to me for any questions or opportunities!</Desc>
         <ContactForm ref={form} onSubmit={handleSubmit}>
           <ContactTitle>Email Me 🚀</ContactTitle>
-          <ContactInput
-            placeholder="Your Email"
-            name="from_email"
-            type="email"
-          />
+          <ContactInput placeholder="Your Email" name="from_email" />
           <ContactInput placeholder="Your Name" name="from_name" />
           <ContactInput placeholder="Subject" name="subject" />
           <ContactInputMessage placeholder="Message" rows="4" name="message" />
-          <ContactButton type="submit">Send</ContactButton>
+          <ContactButton type="submit" value="Send" />
         </ContactForm>
         <Snackbar
           open={open}
           autoHideDuration={6000}
-          onClose={() => setOpen(false)}
+          onClose={()=>setOpen(false)}
           message="Email sent successfully!"
+          severity="success"
         />
       </Wrapper>
     </Container>
-  );
-};
+  )
+}
 
-export default Contact;
+export default Contact
